aee347596e0766633f119eada9b1268b90cd6f6f
SG <sayanghosh.webdev@gmail.com>
2026-05-27 08:46:39 +0530

    refactor: streamline server initialization and remove unused HTTP server setup

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,37 +1,30 @@
 import dotenv from "dotenv";
-import http from "http";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
-import { initSocket } from "./socket/socket.js";
 
-    dotenv.config({
-    path: "./.env"
-    })
+dotenv.config({
+  path: "./.env",
+});
 
-    const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8000;
 
-    // ================= START SERVER =================
-    const startServer = async () => {
-    try {
-        //  CONNECT DB
-        await connectDB();
-        console.log(" MongoDB connected");
+// ================= START SERVER =================
 
-        //  CREATE HTTP SERVER
-        const server = http.createServer(app);
+const startServer = async () => {
+  try {
+    // CONNECT DATABASE
+    await connectDB();
+    console.log("MongoDB connected");
 
-        //  INIT SOCKET
-        initSocket(server);
+    // START EXPRESS SERVER
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
 
-        //  START SERVER
-        server.listen(PORT, () => {
-        console.log(` Server running on port: ${PORT}`);
-        });
-
-    } catch (error) {
-        console.error(" Server start failed:", error.message);
-        process.exit(1);
-    }
+  } catch (error) {
+    console.error("Server start failed:", error.message);
+    process.exit(1);
+  }
 };
 
 startServer();
